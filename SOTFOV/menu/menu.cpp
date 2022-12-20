db410d4a5ea1b6440d11e6828bd9b8fc77d8375a
Valasco <jamesvashro@yahoo.com>
2022-12-19 19:09:26 -0700

    Removed 'FOV text at top left'

--- SOTFOV/menu/menu.cpp
+++ SOTFOV/menu/menu.cpp
@@ -93,7 +93,7 @@ namespace frick {
         ImGui::PushStyleVar(ImGuiStyleVar_GrabRounding, 10);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 5);
 
-        if (ImGui::Begin("FOV        Made by - Valasco#6113", &menu.opened, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar)) {
+        if (ImGui::Begin("            Made by - Valasco#6113", &menu.opened, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar)) {
             if (ImGui::BeginMenuBar()) {
                 if (!vars->simpleMode) {
                     if (ImGui::BeginMenu("File")) {
